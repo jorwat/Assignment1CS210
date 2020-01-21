c2465dec985d16f8d4532a726abe2bf93c3b9764
Shooter2832 <v3y2b@ugrad.cs.ubc.ca>
2020-01-21 15:36:53 -0800

    Created SnowfallLogger constructor and respective functions. Implemented additional tests as well

--- src/main/snowfall/SnowfallLogger.java
+++ src/main/snowfall/SnowfallLogger.java
@@ -3,38 +3,56 @@ package snowfall;
 // Represents a snowfall logger
 public class SnowfallLogger {
     public static final int THRESHOLD = 15;  // threshold snowfall amount in cms for a snow day
+    int record;
+    int day;
+    int snowDay;
+    double average;
 
     // EFFECTS: constructs snowfall logger with no snowfall records
     public SnowfallLogger() {
-        // stub
+        this.record = 0;
+        this.day = 0;
+        this.snowDay = 0;
     }
 
     // REQUIRES: amount > 0
     // MODIFIES: this
     // EFFECTS: records a snowfall of amount centimetres; if amount is at least THRESHOLD cms, records a snow day
     public void addSnowfallRecord(int amount) {
-        // stub
+        if (amount > 0) {
+            this.record = this.record + amount;
+            if (amount >= THRESHOLD) {
+                this.snowDay = this.snowDay + 1;
+            }
+            this.day = this.day + 1;
+        }
     }
 
     // MODIFIES: this
     // EFFECTS: resets the logger so that no snowfall records are recorded
     public void reset() {
-        // stub
+        this.record = 0;
+        this.day = 0;
+        this.snowDay = 0;
     }
 
     // REQUIRES: getNumSnowfallRecords() > 0
     // EFFECTS: returns the average snowfall across all records added
     public double getAverageSnowfall() {
-        return 0.0;   // stub
+        if (getNumSnowfallRecords() > 0) {
+            double answer = this.record;
+            average = answer / day;
+        }
+        return average;
     }
 
     // EFFECTS: returns the number of snowfall records added
     public int getNumSnowfallRecords() {
-        return 0;   // stub
+        return this.day;
     }
 
     // EFFECTS: returns the number of snow days based on snowfall records added
     public int getNumSnowDays() {
-        return 0;  // stub
+        return this.snowDay;
     }
 }
--- src/test/snowfall/SnowfallLoggerTest.java
+++ src/test/snowfall/SnowfallLoggerTest.java
@@ -7,32 +7,74 @@ import static org.junit.jupiter.api.Assertions.*;
 
 // Unit tests for the SnowfallLogger
 class SnowfallLoggerTest {
+    SnowfallLogger l1;
+    SnowfallLogger l2;
 
     @BeforeEach
     void runBefore() {
+     l1 = new SnowfallLogger();
+     l2 = new SnowfallLogger();
     }
 
     @Test
     void testConstructor() {
+        assertEquals(0, l1.getNumSnowfallRecords());
+        assertEquals(0, l1.getAverageSnowfall());
+        assertEquals(0, l1.getNumSnowDays());
     }
 
     @Test
     void testAddSingleSnowfallRecord() {
+        l1.addSnowfallRecord(16);
+        assertEquals(16, l1.record);
+        l2.addSnowfallRecord(14);
+        assertEquals(14, l2.record);
 
     }
 
     @Test
     void testAddMultipleSnowfallRecords() {
+        l1.addSnowfallRecord(16);
+        l1.addSnowfallRecord(14);
+        assertEquals(30, l1.record);
+        l2.addSnowfallRecord(12);
+        l2.addSnowfallRecord(11);
+        assertEquals(23, l2.record);
 
     }
 
     @Test
     void testNumSnowDays() {
-
+        l1.addSnowfallRecord(16);
+        l1.addSnowfallRecord(14);
+        l1.addSnowfallRecord(15);
+        assertEquals(3,l1.day);
+        assertEquals(2,l1.snowDay);
+        l2.addSnowfallRecord(12);
+        l2.addSnowfallRecord(11);
+        l2.addSnowfallRecord(10);
+        assertEquals(3,l2.day);
+        assertEquals(0,l2.snowDay);
     }
 
     @Test
     void testReset() {
+        l1.addSnowfallRecord(16);
+        l1.addSnowfallRecord(14);
+        l1.addSnowfallRecord(15);
+        l1.reset();
+        assertEquals(0, l1.record);
+        assertEquals(0,l1.day);
+        assertEquals(0,l1.snowDay);
+    }
+
+    @Test
+    void testGetAverageSnowfall(){
+        l1.addSnowfallRecord(16);
+        l1.addSnowfallRecord(14);
+        l1.addSnowfallRecord(17);
+        assertEquals(47.0 / 3.0, l1.getAverageSnowfall());
 
     }
+
 }
